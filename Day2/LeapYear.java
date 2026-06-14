5800ef1af6f1a45e51b705258deff0cff4854384
Aditya Kesarwani <adityakes41@gmail.com>
2026-06-14 23:07:31 +0530

    added program

---
+++ Day2/LeapYear.java
@@ -0,0 +1,20 @@
+//package Day2;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter a year");
+    int year = sc.nextInt();
+    if(year % 400 == 0)
+        System.out.println("This year is leap year");
+    else if(year % 100 == 0)
+        System.out.println("This year is not leap year");
+    else if(year % 4 == 0)
+        System.out.println("This year is leap year");
+    else
+        System.out.println("This year is not leap year");
+
+}
+}
