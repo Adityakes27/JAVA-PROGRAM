be2a29663475156719b13763ea41147fd3837f06
Aditya Kesarwani <adityakes41@gmail.com>
2026-06-15 23:38:57 +0530

    added program

--- PrintNumFrom1TON.java
+++ PrintNumFrom1TON.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PrintNumFrom1ToN {
+public class PrintNumFrom1TON {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
